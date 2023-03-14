0bda1d24c4b38b9afe0b085badce02e1c20eb615
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-03-14 21:19:40 +0300

    Add script to array.js

---
+++ array.js
@@ -0,0 +1,28 @@
+let scores = [60, 50, 60, 58, 54, 54, // new array
+              58, 50, 52, 54, 48, 69,
+              34, 55, 51, 52, 44, 51,
+              69, 64, 66, 55, 52, 61,
+              46, 31, 57, 52, 44, 18,
+              41, 53, 55, 61, 51, 44];
+
+let highScore = 0;
+let output;
+
+for (let i = 0; i < scores.length; i++) { // iteration array elements
+
+    output = "Bubble solution " + i + " score: " + scores[i]; // output results
+    console.log(output);
+    if (scores[i] > highScore) {
+        highScore = scores[i];
+    }
+}
+console.log("Bubbles tests: " + scores.length);
+console.log("Highest bubbles score: " + highScore);
+
+let bestSolutions = []; // empty array for new results
+for (let i = 0; i < scores.length; i++) {
+    if (scores[i] == highScore) {
+        bestSolutions.push(i);
+    }
+}
+console.log("Solutions with the highest score: " + bestSolutions);
