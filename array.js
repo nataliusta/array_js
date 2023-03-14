27fcba09423251dd8961ee88dc572d9376cc24c7
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-03-15 00:37:30 +0300

    Add comments

--- array.js
+++ array.js
@@ -5,24 +5,24 @@ let scores = [60, 50, 60, 58, 54, 54, // new array
               46, 31, 57, 52, 44, 18,
               41, 53, 55, 61, 51, 44];
 
-let highScore = 0;
+let highScore = 0; // max result with default value zero
 let output;
 
 for (let i = 0; i < scores.length; i++) { // iteration array elements
 
     output = "Bubble solution " + i + " score: " + scores[i]; // output results
     console.log(output);
-    if (scores[i] > highScore) {
+    if (scores[i] > highScore) { // if current result more than max result (highScore) it becomes a new highScore
         highScore = scores[i];
     }
 }
 console.log("Bubbles tests: " + scores.length);
 console.log("Highest bubbles score: " + highScore);
 
-let bestSolutions = []; // empty array for new results
+let bestSolutions = []; // empty array for new max results
 for (let i = 0; i < scores.length; i++) {
-    if (scores[i] == highScore) {
-        bestSolutions.push(i);
+    if (scores[i] == highScore) { // in case current result = max result (highScore) add i to bestSolutions
+        bestSolutions.push(i); // add new index to array
     }
 }
 console.log("Solutions with the highest score: " + bestSolutions);
