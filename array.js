af21c93a94cf6f02f5a16c43c53ea6706ce81830
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-03-22 00:01:12 +0300

    Add function getMostCostEffectiveSolution

--- array.js
+++ array.js
@@ -47,4 +47,20 @@ function getBestResults(scores, highScore) {
 let bestSolutions = getBestResults(scores, highScore);
 console.log("Solutions with the highest score: " + bestSolutions);
 
-function getMostCostEffectiveSolution (score, costs, highScore)
+function getMostCostEffectiveSolution (scores, costs, highScore) {
+    let cost = 100;
+    let index;
+
+    for (let i = 0; i < scores.length; i++) {
+        if (scores[i] == highScore) {
+            if (cost > costs[i]) {
+                index = i;
+                cost = costs[i];
+            }
+        }
+    }
+    return index;
+}
+
+let mostCostEffective = getMostCostEffectiveSolution(scores, costs, highScore);
+console.log('Bubble Solution ' + mostCostEffective + ' is the most cost effective');
